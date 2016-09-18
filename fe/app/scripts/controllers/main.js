8a4bfd6d93fd710b9795ed078672736e2d8723b9
dongxu <i@huangdx.net>
2016-09-18 20:37:28 +0800

    add fe

---
+++ fe/app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the feApp
+ */
+angular.module('feApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.count = 1025;
+  });
